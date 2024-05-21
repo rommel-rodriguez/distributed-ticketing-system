4fcfa8ed93542f6d79f2ae4958ae961be5051fd6
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-05-21 12:30:16 -0500

    Made routes available to express app

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
 
 const app = express();
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
 
 app.get('/api/users/currentuser', (req, res) => {
   res.send('Das muessen die Daten des Benutzerz sein!!!!');
---
+++ auth/src/routes/signout.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/signout', (req, res) => {
+  res.send('Hallo!');
+});
+
+export { router as currentUserRouter };
