e967988add577eca8d2a49285b33c54e6b59262d
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-06-15 09:04:41 -0500

    Added support JWT authenticatioon using cookies

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -33,6 +34,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'asdf'
+    );
+
+    // Store userJwt on session object from cookie-session
+    // This is different than the official documentation to circumvent a compatibility
+    // issue with TS.
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
