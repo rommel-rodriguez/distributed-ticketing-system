cbea4493f448091c224bd9c2550aed4e57180032
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-06-24 09:26:16 -0500

    Finished implementing basic app service

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -4,12 +4,13 @@ import { app } from '../../app';
 const user = { email: 'test@test.com', password: 'password' };
 
 it('Responds with details about the current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send(user)
-    .expect(201);
+  // const authResponse = await request(app)
+  //   .post('/api/users/signup')
+  //   .send(user)
+  //   .expect(201);
 
-  const cookie = authResponse.get('Set-Cookie');
+  // const cookie = authResponse.get('Set-Cookie');
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get('/api/users/currentuser')
@@ -17,5 +18,13 @@ it('Responds with details about the current user', async () => {
     .send()
     .expect(200);
 
-  console.log(response.body);
+  expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('Responds with null if the user is not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(401);
+  expect(response.body.currentUser).toEqual(undefined);
 });
