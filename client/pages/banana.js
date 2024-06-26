789bbbefa05f58f5786f76a8977a15ea5a4a5108
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-06-26 09:01:44 -0500

    Basic setup for client app/service done

---
+++ client/pages/banana.js
@@ -0,0 +1,5 @@
+const bananaPage = () => {
+  return <h1>This is a Banana2 Page!</h1>;
+};
+
+export default bananaPage;
---
+++ client/pages/index.js
@@ -0,0 +1,5 @@
+const homePage = () => {
+  return <h1>Landing Page</h1>;
+};
+
+export default homePage;
