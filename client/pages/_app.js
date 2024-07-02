83fd1245e6ed5a90f8352524946cf635b150d46f
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-07-02 17:52:53 -0500

    Added Header component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <h1>Header!</h1>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </div>
   );
@@ -15,10 +16,17 @@ AppComponent.getInitialProps = async (appContext) => {
   // as argument, differs slightly.
   console.log(appContext);
   const client = buildClient(appContext.ctx);
-  const { data } = await client.get('/api/users/currentuser');
-  const pageProps = await appContext.Component.getInitialProps(appContext.ctx);
-
-  return data;
+  const { data } = (await client.get('/api/users/currentuser').catch((err) => {
+    console.error(err.message);
+    console.error('Error Data:');
+    console.log(err.response?.data);
+  })) || { data: { currentUser: null } };
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  console.log(pageProps);
+  return { pageProps, ...data };
 };
 
 export default AppComponent;
