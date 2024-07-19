9a83d54d57827499c05402bab2f012e713c9a214
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-07-18 19:21:20 -0500

    Working prototypes for NATS JetStream use are ready
    Both the publisher and listener (consumer) script are working properly
    for NATS JetStream version 2.10.17 on alpine 3.20 (docker images).

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -9,13 +9,24 @@ const NATSJS_HOST = 'localhost:4222';
   // Create a JetStream context
   const js = nc.jetstream();
 
+  // js.consumers.get('foo')
+
   // Create a StringCodec for encoding/decoding messages
   const sc = StringCodec();
+  // TODO: Retrieve all previous messages in the topic
+  // console.log('Streaming messages: ');
+  // // Subscribe to the stream and receive messages
+  // const sub = nc.subscribe('foo');
+  // for await (const m of sub) {
+  //   console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
 
-  // Subscribe to the stream and receive messages
-  const sub = nc.subscribe('replace-topic-here');
-  for await (const m of sub) {
-    console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
+  // }
+  const c = await js.consumers.get('mystream');
+  const messages = await c.consume();
+  for await (const m of messages) {
+    // console.log(m.seq);
+    console.log(sc.decode(m.data));
+    m.ack();
   }
   console.log('subscription closed');
   await nc.close();
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,11 @@
-import { connect, StringCodec, JetStreamManager } from 'nats';
+import {
+  connect,
+  StringCodec,
+  JetStreamManager,
+  RetentionPolicy,
+  StorageType,
+  DiscardPolicy,
+} from 'nats';
 
 const NATSJS_HOST = 'localhost:4222';
 
@@ -6,10 +13,27 @@ const NATSJS_HOST = 'localhost:4222';
   // Connect to the NATS server
   const nc = await connect({ servers: `nats://${NATSJS_HOST}` });
 
+  console.log('Publisher connected to NATS');
+  console.log(nc);
   // Create a JetStream manager
   const jsm: JetStreamManager = await nc.jetstreamManager();
+
+  // NOTE: A Stream can contain many topics.
+  // Stream definition
+  const streamConfig = {
+    name: 'mystream',
+    subjects: ['foo'],
+    storage: StorageType.File, // Use file storage
+    retention: RetentionPolicy.Limits, // Use limits for retention policy
+    max_age: 0, // Messages never expire
+    max_bytes: -1, // No limit on total stream size
+    max_msgs: -1, // No limit on the number of messages
+    discard: DiscardPolicy.Old, // Discard old messages when the limit is reached
+    duplicates: 120000000000, // 2 minutes for duplicate tracking window
+  };
   // Create a stream (if not already exists)
-  await jsm.streams.add({ name: 'mystream', subjects: ['foo'] });
+  // await jsm.streams.add({ name: 'mystream', subjects: ['foo'] });
+  await jsm.streams.add(streamConfig);
 
   // Create a JetStream context
   const js = nc.jetstream();
