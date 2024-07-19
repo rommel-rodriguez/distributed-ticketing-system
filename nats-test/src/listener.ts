e9d0579c61d94a6e45b76b87f5136026e15fae06
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-07-19 15:46:29 -0500

    Trying to get the desired behavior from the listener prototype
    The problem is that Jetstream workqueues only accept, at least it seems,
    either a single consumer or consumers with different filters (for
    different topics). But, the behavior I want to achieve right now, is
    intended for me to be able to horizontally scale the services and that,
    at least for the particular use case I am prototyping right now,
    messages are only consumed/send-to a single instance/pod of a
    service/deployment, and all of them must be subscribed to the same queue
    (workqueue in jetstream terms).

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,33 +1,69 @@
-import { connect, StringCodec, JetStreamManager } from 'nats';
+import { randomBytes } from 'crypto';
+import {
+  connect,
+  StringCodec,
+  JSONCodec,
+  JetStreamManager,
+  AckPolicy,
+} from 'nats';
 
+// console.clear();
 const NATSJS_HOST = 'localhost:4222';
+
+interface EventData {
+  id: string;
+  title: string;
+  price: number;
+}
 
 (async () => {
   // Connect to the NATS server
   const nc = await connect({ servers: `nats://${NATSJS_HOST}` });
 
+  const jsm: JetStreamManager = await nc.jetstreamManager();
   // Create a JetStream context
   const js = nc.jetstream();
 
-  // js.consumers.get('foo')
+  const sc = JSONCodec();
 
-  // Create a StringCodec for encoding/decoding messages
-  const sc = StringCodec();
-  // TODO: Retrieve all previous messages in the topic
-  // console.log('Streaming messages: ');
-  // // Subscribe to the stream and receive messages
-  // const sub = nc.subscribe('foo');
-  // for await (const m of sub) {
-  //   console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
+  // NOTE: Consumer version: consume everything in the stream
+  // const c = await js.consumers.get('event-stream', {
+  //   name_prefix: 'listener01',
+  //   filterSubjects: 'ticket:created',
+  // });
+  // const c = await js.consumers.get('test-stream', {
+  //   name_prefix: 'listener01',
+  //   filterSubjects: 'ticket:created',
+  // });
+  // NOTE:  This version creates a 'durable' consumer
+  // generate random durable name for the consumer
+  const durableWorkerName = `shadow-worker-${randomBytes(20).toString('hex')}`;
+  // Creates the durable cosumer
+  await jsm.consumers.add('test-stream', {
+    durable_name: durableWorkerName,
+    ack_policy: AckPolicy.Explicit,
+    filter_subject: 'ticket:create',
+  });
 
-  // }
-  const c = await js.consumers.get('mystream');
+  const c = await js.consumers.get('test-stream', durableWorkerName);
   const messages = await c.consume();
   for await (const m of messages) {
     // console.log(m.seq);
-    console.log(sc.decode(m.data));
+    let payload: EventData = sc.decode(m.data) as EventData;
+    console.log(
+      `Title: ${payload.title},Subject: ${m.subject}, Seq: ${m.seq}, Listener: ${m.info.consumer}`
+    );
+    // console.log('TEST');
+    // console.log(m);
+    console.log(payload);
     m.ack();
   }
+  // NOTE: Subscription version: Consume just the most recent, or from a work queue, just he un-acknowledged
   console.log('subscription closed');
+  // const sub = nc.subscribe('tickets:created');
+  // for await (const m of sub) {
+  //   console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
+  // }
+  // console.log('subscription closed');
   await nc.close();
 })();
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,17 +1,23 @@
 import {
   connect,
-  StringCodec,
+  PubAck,
+  JSONCodec,
   JetStreamManager,
   RetentionPolicy,
   StorageType,
   DiscardPolicy,
 } from 'nats';
 
+console.clear();
+
 const NATSJS_HOST = 'localhost:4222';
 
 (async () => {
   // Connect to the NATS server
-  const nc = await connect({ servers: `nats://${NATSJS_HOST}` });
+  const nc = await connect({
+    servers: `nats://${NATSJS_HOST}`,
+    name: 'client02',
+  });
 
   console.log('Publisher connected to NATS');
   console.log(nc);
@@ -21,15 +27,17 @@ const NATSJS_HOST = 'localhost:4222';
   // NOTE: A Stream can contain many topics.
   // Stream definition
   const streamConfig = {
-    name: 'mystream',
-    subjects: ['foo'],
+    // name: 'event-stream',
+    name: 'test-stream',
+    subjects: ['ticket:created'],
     storage: StorageType.File, // Use file storage
-    retention: RetentionPolicy.Limits, // Use limits for retention policy
-    max_age: 0, // Messages never expire
-    max_bytes: -1, // No limit on total stream size
-    max_msgs: -1, // No limit on the number of messages
+    retention: RetentionPolicy.Workqueue, // Use limits for retention policy
+    // retention: RetentionPolicy.Limits, // Use limits for retention policy
+    // max_age: 0, // Messages never expire
+    // max_bytes: -1, // No limit on total stream size
+    // max_msgs: -1, // No limit on the number of messages
     discard: DiscardPolicy.Old, // Discard old messages when the limit is reached
-    duplicates: 120000000000, // 2 minutes for duplicate tracking window
+    // duplicates: 120000000000, // 2 minutes for duplicate tracking window
   };
   // Create a stream (if not already exists)
   // await jsm.streams.add({ name: 'mystream', subjects: ['foo'] });
@@ -39,11 +47,17 @@ const NATSJS_HOST = 'localhost:4222';
   const js = nc.jetstream();
 
   // Create a StringCodec for encoding/decoding messages
-  const sc = StringCodec();
+  const sc = JSONCodec();
+
+  const data = { id: '123', title: 'concert', price: 20 };
 
   // Publish a message to the stream
-  const pa = await js.publish('foo', sc.encode('Hello World!'));
-  console.log(`Published message with sequence: ${pa.seq}`);
+  const pa: PubAck = await js.publish('ticket:created', sc.encode(data)); // NOTE: Returns an Acknowledgement like object
+  if (pa) {
+    console.log(`Published message with sequence: ${pa.seq}`);
+    console.log('Whole of PA object:');
+    console.log(pa);
+  }
 
   await nc.close();
 })();
