df517d6c27631ec0cd45c59d1d797cdd728db6a8
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-11-20 16:41:18 -0500

    Added note on how the nats-wrapper mock should be implemented
    For my case, this object must have a 'connection' property of type NatsConnection
    and such an object must implement a method that will
    return a JetStreamClient object.

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -1,1 +1,9 @@
-export const natsWrapper = {};
+export const natsWrapper = {
+  /**
+   * For my case, this object must have a 'connection' property of type NatsConnection
+   * and such an object must implement a 'jetstream' method that will
+   * return a JetStreamClient object. The client object must implement the publish
+   * method
+   */
+  connection: {},
+};
