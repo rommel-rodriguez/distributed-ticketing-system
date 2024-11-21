3fe23193dd42b85cb7b1d05f8bd5140987550367
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-11-21 07:32:18 -0500

    Added functionality to test whether the stream already exists
    More accurately, test if the stream has already been configured on the
    server. If the stream already exists, the code assumes that the stream
    has been also configured with the appropriate subjects.
    
    As a side note, it might, or might not, be a good idea to let services
    register their own events by means of editing the nats stream
    configuration. Let it be noted that doing this might not be compatible
    with other message-passing software.

--- common/src/events/event-setup.ts
+++ common/src/events/event-setup.ts
@@ -1,15 +1,46 @@
 import {
-  connect,
-  PubAck,
-  JSONCodec,
   JetStreamManager,
   RetentionPolicy,
   StorageType,
-  DiscardPolicy,
   NatsConnection,
+  NatsError,
 } from 'nats';
 
-const subjects = ['ticket:created'];
+import { Subjects } from './subjects';
+
+/**
+ * Finds out if the stream already exists
+ * @param stream  - A stream representing the name of the stream
+ * @param jsm - A JetStreamManager-type object
+ * @returns  true, if the stream exists, false if it doesn't
+ */
+const streamAlreadyExists = async (
+  stream: string,
+  jsm: JetStreamManager
+): Promise<boolean> => {
+  let streamInfo;
+  try {
+    streamInfo = await jsm.streams.info(stream);
+  } catch (err) {
+    if (err instanceof NatsError) {
+      console.log('[DEBUG] The stream ' + stream + ' does NOT exists yet.');
+      return false;
+    }
+    // Re-throw the error if its not a NatsError (handled by our custom error middleware)
+    throw err;
+  }
+  console.log(`[DEBUG] Does the stream ${stream} exists?\nGot:\n`);
+  console.log(streamInfo);
+
+  // if (streamInfo) return true;
+
+  return true;
+};
+
+// TODO: Analyze whether or not it is a good design decision to leave the specification
+// , or initialization, of subjects here. Maybe..
+//  const subjects = ['ticket:created', 'ticket:updated'];
+const subjects = [Subjects.TicketCreated, Subjects.TicketUpdated];
 
 /**
  * Creates a persistent NATS JS stream and associates an array of subjects with it.
@@ -21,6 +52,16 @@ const setupEventStream = async (nc: NatsConnection, subjects: string[]) => {
   const streamName = 'event-stream';
   const jsm: JetStreamManager = await nc.jetstreamManager();
 
+  const streamExists = await streamAlreadyExists(streamName, jsm);
+
+  if (streamExists) {
+    console.log(`The stream ${streamName} already exists`);
+    console.log(
+      'Assuming that it is properly configured with the needed subjects'
+    );
+    return;
+  }
+
   const streamConfig = {
     name: streamName,
     subjects: subjects,
@@ -30,7 +71,8 @@ const setupEventStream = async (nc: NatsConnection, subjects: string[]) => {
     max_msgs: -1, // No limit on the number of messages
   };
   const streamInfo = await jsm.streams.add(streamConfig);
-  console.log('Stream "add" returned:\n', streamInfo);
+  // console.log('[DEBUG] Stream "add" returned:');
+  console.log('Stream Added and configured. Stream configuration:\n');
   console.log(streamInfo);
 };
 
