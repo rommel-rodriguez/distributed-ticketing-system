ba9707cb592b4c8dec3f3401fb2e6f4a2ff70fc2
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-11-29 17:31:57 -0500

    Starting code for the new Orders service
    Basically just copied and modified code form the tickets service.

---
+++ orders/src/index.ts
@@ -0,0 +1,81 @@
+import mongoose from 'mongoose';
+import { natsWrapper } from './nats-wrapper';
+import { NatsConnection } from 'nats';
+
+import { app } from './app';
+
+const NATSJS_HOST = 'nats-jets-svc:4222';
+const natsUrl = `nats://${NATSJS_HOST}`;
+
+type TypeOfNatsWrapper = typeof natsWrapper;
+
+const handleNatsClose = async (natsWrapper: TypeOfNatsWrapper) => {
+  let natsConnectionClosed;
+  try {
+    natsConnectionClosed = natsWrapper.connection.closed();
+  } catch (error) {
+    console.log('Inside NATS Connection Closed Handler');
+    console.log('Something went wrong while trying to access the connection');
+    console.log(error);
+    process.exit();
+  }
+  while (true) {
+    const err = await natsConnectionClosed;
+    console.log('Connection to NATS Server closed!!!');
+    console.log(err);
+    process.exit();
+  }
+};
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('The private key for JWT Tokens not set');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('Mongo URI must be defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+  try {
+    await natsWrapper.connect(process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+    // NOTE: I have a problem. Here is the place in which I should place
+    // the code to handle kb interrupts and to exit the app if we fail to
+    // connect to nats, which is done through a callback, well, this is
+    // not quite the same for the version of NATS JS I am currently using.
+    // TODO: Implement code to handle interrupts and to terminate the
+    // application in case we can not connect to a NATS server.
+    // const natsConnectionClosed = natsWrapper.connection.closed();
+    // TODO: With this flow, handleNatsClose assumes that a connection
+    // will be stablished prior to execution. However, if NATS fails
+    // to start by the next time the tickets server restarts (through k8s)
+    // then we get an error, due to wanting to access a connection that
+    // was never started in the first place.
+    handleNatsClose(natsWrapper);
+    process.on('SIGINT', async () => await natsWrapper.connection.close());
+    process.on('SIGTERM', async () => await natsWrapper.connection.close());
+    // NOTE: natsConnectionClosed  gets resolves to a value when the connection is
+    // closes, but, where do I 'await' that value? Maybe at the very end of this block?
+    // because, if I put it bedore mongoose, as we are inside a the async block,
+    // mongoose.connect will never run until we get something from close()!!!
+    // Also, I would need this function to run to forever at least logging whatever
+    // made the nats connection to be lost.
+    await natsWrapper.setupStream();
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB!!!');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+app.listen(3000, () => {
+  console.log('=> Lauschen auf Port 3000!!!');
+});
+
+start();
---
+++ orders/src/nats-wrapper.ts
@@ -0,0 +1,59 @@
+import { NatsConnection, connect, JetStreamClient } from 'nats';
+import { setupEventStreamWrapper } from '@rrpereztickets/common';
+
+const NATSJS_HOST = 'localhost:4222';
+
+class NatsWrapper {
+  private _connection?: NatsConnection;
+  private _client?: JetStreamClient;
+
+  /**
+   * NOTE: Let it be noted, that wih my current implementation, I am no passing a
+   * nats client object but a nats connection type object. So, for each call tot he
+   * publish method in a publisher, a new client object will be created. This might
+   * not be a problem for a test project, but further compare the overhead of doing this
+   * versus instantiating a client and passing that around instead of a raw connection.
+   */
+  get connection() {
+    if (!this._connection) {
+      throw new Error(
+        'Cannot access NATS connection calling the connect method'
+      );
+    }
+    return this._connection;
+  }
+
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    }
+    return this._client;
+  }
+
+  async connect(clientId: string, url: string) {
+    try {
+      this._connection = await connect({
+        // servers: `nats://${url}`,
+        servers: url,
+        // name: `events-client-${randomBytes(3).toString('hex')}`,
+        name: clientId,
+        timeout: 500,
+      });
+      console.log('Successfully connected to NATS server');
+      this._client = this._connection.jetstream();
+      console.log('Successfully created NATS client');
+    } catch (error) {
+      console.log('Error while connecting to NATS server');
+      console.log(error);
+    }
+  }
+
+  async setupStream() {
+    if (!this._connection) {
+      throw new Error('Cannot setup stream before connecting to NATS');
+    }
+    await setupEventStreamWrapper(this._connection);
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
