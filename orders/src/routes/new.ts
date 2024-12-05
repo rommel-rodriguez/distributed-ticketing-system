986a4927b8f6fe8a07a46097f7d7ce6773203e34
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-12-05 10:39:02 -0500

    Added middlewares to Order's 'new' Route
    Also, added the following line:
        .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
    Just as a demo of how to create a custom validation. As this introduces
    coupling between service, it can be removed at anytime.

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,19 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@rrpereztickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
-router.get('/api/orders'),
+router.post('/api/orders'),
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     res.send({});
   };
