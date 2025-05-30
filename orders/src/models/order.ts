1eaf4b4e3866ed59e5a64342258456150721920c
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2025-05-30 09:16:13 -0500

    Added the version attribute to the Order model
    As with the Ticket model in its domain service (tickets), this field was
    added to the Order model to implement Optimistic Concurrency Control for
    the Order model as well.

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@rrpereztickets/common';
 import { TicketDoc } from './ticket';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 export { OrderStatus };
 
@@ -59,6 +60,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
