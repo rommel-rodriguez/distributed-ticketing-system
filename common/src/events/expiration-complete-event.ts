6b7bf56ce27362788d20f6ed394f5004fc5a6d07
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2025-06-05 18:47:14 -0500

    Fixed ExpirationCompleteEvent's name
    Was missing the 'Event' suffix

--- common/src/events/expiration-complete-event.ts
+++ common/src/events/expiration-complete-event.ts
@@ -2,7 +2,7 @@ import { Subjects } from './subjects';
 import { Event } from './nats-listener';
 import { Streams } from './streams';
 
-export interface ExpirationComplete extends Event {
+export interface ExpirationCompleteEvent extends Event {
   stream: Streams.EventStream;
   subject: Subjects.TicketCreated;
   data: {
