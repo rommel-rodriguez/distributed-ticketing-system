8e1bc68200c79e38911bda92760f510bf8899ed8
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2025-06-08 09:45:41 -0500

    Finished building the Expiration Service

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -27,7 +27,10 @@ export class OrderCreatedListener extends NatsListener<OrderCreatedEvent> {
     const delay =
       new Date(decodedData.expiresAt).getTime() - new Date().getTime();
 
-    await expirationQueue.add({ orderId: decodedData.id }, { delay });
+    await expirationQueue.add(
+      { orderId: decodedData.id }
+      //  { delay }
+    );
 
     message.ack();
   }
