c38c9391aa9c238c7323e3a42bd9d54e6e2dd1af
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2025-08-18 10:44:06 -0500

    Modifying the new route for Stripe Payment Intents
    The new recommended workflow for Stripe PIs will require some changes.
    The use of 'Charges' is now deprecated, and all tho as similar method
    exists that is akin to the 'Charges+Token' method, the recommended
    method right now is simpler workflow in which we only interact directly
    with our client application once. After, the payment confirmation, is
    returned to us via a our webhook as an State Machine.

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,8 @@ import {
   OrderStatus,
 } from '@rrpereztickets/common';
 
+import { stripe } from '../stripe';
+
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -17,10 +19,14 @@ const router = express.Router();
 router.post(
   '/api/payments',
   requireAuth,
-  [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
+  [body('orderId').not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
     const { token, orderId } = req.body;
+    // NOTE: I am just going to define currency here because so far I was not
+    // expecting to use payment intents. But, this should come from either the request's
+    // body or is information stored within the order.
+    const currency = 'usd';
 
     const order = await Order.findById(orderId);
 
@@ -34,6 +40,23 @@ router.post(
 
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for a cancelled order');
+    }
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create(
+        {
+          amount: order.price * 100,
+          currency,
+          customer: order.userId,
+          automatic_payment_methods: { enabled: true },
+          // TODO: Set a 'return_url' here, so that in the case the client is presented
+          // with a 3ds challenge the client returns to our designated location
+        },
+        { idempotencyKey: req.header('Idempotency-Key') ?? undefined }
+      );
+      res.json({ clientSecret: paymentIntent.client_secret });
+    } catch (err: any) {
+      res.status(400).json({ error: err.message });
     }
 
     res.send({ success: true });
