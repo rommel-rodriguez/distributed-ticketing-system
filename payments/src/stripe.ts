28e7a944ee1be9bc6eca3fc7bc4c33ddd7d88518
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2025-09-18 16:04:23 -0500

    Added K8s config for stripe's webhook secret
    Also modified the apiVersion line in stripe.ts. This was needed because
    of a mismatch between the string literal VSCode's linter expected and
    the one that the compiler expected. The new string literal is what the
    compiler expects to encounter.
    
    A future improvement would be to store this api version string inside an
    environment variable.

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,5 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_KEY!, {
-  apiVersion: '2025-05-28.basil',
+  //apiVersion: '2025-05-28.basil',
+  // apiVersion: '2025-07-30.basil',
+  apiVersion: '2025-07-30.basil' as unknown as Stripe.LatestApiVersion,
 });
