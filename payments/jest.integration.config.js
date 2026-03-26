67dc4f61a59a36a8f750bb6387eee3f5cfa0ffa2
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-03-26 09:19:46 -0500

    Update: name of tests to simpler pattern
    Now that the test are segregated by directory, there is little to no
    value in keeping the having the type of test as a part of their name.

--- payments/jest.integration.config.js
+++ payments/jest.integration.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    '<rootDir>/src/services/**/__tests__/integration/*.service.integration.test.ts',
+    '<rootDir>/src/services/**/__tests__/integration/*.service.test.ts',
   ],
   setupFilesAfterEnv: ['<rootDir>/src/test/integration/setup.compose.ts'],
   maxWorkers: 1, // Important for deterministic concurrency tests + less flakiness
