8a0dae457ad36d1e0c5890a5c11f3271c07f9e24
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2026-03-25 23:23:27 -0500

    Refactor: naming and directory structure for tests
    Now, while the tests are still collocated, now the tests are further
    split by directory in: unit, integration, system and e2e.

--- payments/jest.integration.config.js
+++ payments/jest.integration.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.integration.test.ts'],
-  setupFilesAfterEnv: ['<rootDir>/src/test/integration/setup.ts'],
+  testMatch: [
+    '<rootDir>/src/services/**/__tests__/integration/*.service.integration.test.ts',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test/integration/setup.compose.ts'],
   maxWorkers: 1, // Important for deterministic concurrency tests + less flakiness
   testTimeout: 120000,
 };
